8182ca37ff74c0477b3dff8208e184b4b94f70a2
KirpichOrig <bdanil0@mail.ru>
2024-11-01 12:51:57 +0300

    first commit

---
+++ src/productsData.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: 1,
+    image: '/catalog/oil.jpg',
+    title: 'Набор машинных масел',
+    price: 335,
+    stock: 5,
+    category: 'Масло'
+  },
+  {
+    id: 2,
+    image: '/catalog/oil.webp',
+    title: 'Машинное масло Mobil Delvac',
+    price: 300,
+    stock: 0,
+    category: 'Масло' // изменено на 'Диффузоры'
+  },
+  {
+    id: 3,
+    image: '/catalog/колеса.jpg',
+    title: 'Колеса Ford Focus',
+    price: 250,
+    stock: 2,
+    category: 'Колеса/Диски' // изменено на 'Диффузоры'
+  },
+  {
+    id: 4,
+    image: '/catalog/диски1.jpg',
+    title: 'Диски комплект',
+    price: 350,
+    stock: 3,
+    category: 'Колеса/Диски' // изменено на 'Диффузоры'
+  },
+  {
+    id: 5,
+    image: '/catalog/диски2.webp',
+    title: 'Диски',
+    price: 320,
+    stock: 0,
+    category: 'Колеса/Диски' // изменено на 'Диффузоры'
+  },
+  {
+    id: 6,
+    image: '/catalog/диски3.jpeg',
+    title: 'Диски черные',
+    price: 320,
+    stock: 1,
+    category: 'Колеса/Диски' // изменено на 'Диффузоры'
+  },
+];
+
+export default products;
